f3131a7e4256473a08834c4fef3d8ad177cc5999
Daniel Burrows <dburrows@debian.org>
2005-10-12 18:52:45 +0000

    [aptitude @ Add GPL boilerplate]

--- src/pkg_sortpolicy.h
+++ src/pkg_sortpolicy.h
@@ -1,6 +1,22 @@
 // pkg_sortpolicy.h    (-*-c++-*-)
 //
-//  Copyright 2001 Daniel Burrows
+//   Copyright (C) 2001, 2005 Daniel Burrows
+//
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of
+//   the License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; see the file COPYING.  If not, write to
+//   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+//   Boston, MA 02111-1307, USA.
+//
 //
 // Package sorting policies.  Currently they just handle PkgIterator/VerIterator
 // pairs and a wrapper class integrates them with vs_tree.  If this can handle
