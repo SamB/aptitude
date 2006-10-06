d7be05dba1c68621f550a80613ab9f589dc3bcf2
Daniel Burrows <Daniel_Burrows@alumni.brown.edu>
2006-10-06 02:47:52 +0000

    [aptitude @ Remove a reference in a comment to vs_minibuf, which no longer exists.]

--- src/vs_progress.cc
+++ src/vs_progress.cc
@@ -16,8 +16,6 @@
 //  along with this program; see the file COPYING.  If not, write to
 //  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 //  Boston, MA 02111-1307, USA.
-//
-//  A vs_minibuf that also acts as a progress bar.
 
 #include "aptitude.h"
 
