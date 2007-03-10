6f171615ff670d81c6e9b4c7769b38b82e2539c7
Daniel Burrows <Daniel_Burrows@alumni.brown.edu>
2007-03-10 18:15:44 +0000

    [aptitude @ Fix ssprintf to actually work when generating big strings (it was allocating a bigger buffer, but truncating the string at the small buffer's size!)]

--- src/generic/util/util.cc
+++ src/generic/util/util.cc
@@ -1,6 +1,6 @@
 // util.cc
 //
-//   Copyright (C) 2005 Daniel Burrows
+//   Copyright (C) 2005, 2007 Daniel Burrows
 //
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of the GNU General Public License as
@@ -64,17 +64,18 @@ const int initbufsize=512;
 string vssprintf(const char *format, va_list ap)
 {
   char buf[initbufsize];
-  int amt = vsnprintf(buf, initbufsize, format, ap);
+  const int amt = vsnprintf(buf, initbufsize, format, ap);
 
   if(amt < initbufsize)
     return buf;
   else
     {
-      char *buf2 = new char[amt+1];
+      const int buf2size = amt + 1;
+      char *buf2 = new char[buf2size];
 
-      int amt2 = vsnprintf(buf2, initbufsize, format, ap);
+      const int amt2 = vsnprintf(buf2, buf2size, format, ap);
 
-      eassert(amt2 < amt+1);
+      eassert(amt2 < buf2size);
 
       string rval(buf2, amt2);
 
