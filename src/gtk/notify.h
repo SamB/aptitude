1982174d3c86b7854853e9e76bf345b0b178a75d
Daniel Burrows <dburrows@debian.org>
2008-08-13 19:21:12 -0700

    Add some doccomments to the NotifyView.

--- src/gtk/notify.h
+++ src/gtk/notify.h
@@ -36,17 +36,45 @@ namespace gui
       Notification(const Glib::RefPtr<Gtk::TextBuffer> buffer, std::vector<Gtk::Button *> buttons);
   };
 
+  /** \brief Stores a stack of global and tab-local notifications.
+   *
+   *  Each notification is just a bunch of widgets packed into a box.
+   *  The boxes are stacked vertically, with global notifications
+   *  appearing below tab-local ones.
+   */
   class NotifyView : public Gtk::VBox
   {
     private:
       Gtk::VBox * rows;
+      // Stores all the notifications that are present in this view.
+      // Notifications
       std::vector<std::pair<Tab *, Notification *> > notifications_map;
       Tab * current_tab;
+
     public:
       NotifyView(BaseObjectType* cobject, const Glib::RefPtr<Gnome::Glade::Xml>& refGlade);
       ~NotifyView();
+      /** \brief Add a new global notification.
+       *
+       *  \param notification   The notification to add.
+       *  The NotifyView will take ownership of this pointer.
+       */
       void add_global_notification(Notification * notification);
+      /** \brief Add a new tab-local notification.
+       *
+       *  The notification is owned by this view after being added.
+       *
+       *  \param notification   The notification to add.
+       *  The NotifyView will take ownership of this pointer.
+       *  \param tab            The tab to add the notification for.
+       *  The notification will be displayed only when this tab is the
+       *  currently active one in the main notebook.
+       */
       void add_local_notification(Notification * notification, Tab * tab);
+      /** \brief Remove a notification.
+       *
+       *  \param notification   The notification to remove.
+       */
       void remove_notification(Notification * notification);
       void tab_changed(Tab * tab);
       Gtk::VBox * get_rows() { return rows; };
