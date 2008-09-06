cfeeac6923cfc955535207daafd2376273a25d8e
Daniel Burrows <dburrows@debian.org>
2008-09-06 12:34:19 -0700

    Fix the file metadata and write doccomments for parse.h.

--- src/generic/apt/matching/parse.h
+++ src/generic/apt/matching/parse.h
@@ -1,4 +1,4 @@
-// matchers.h  -*-c++-*-
+// parse.h  -*-c++-*-
 //
 //  Copyright 2000-2001, 2005, 2007-2008 Daniel Burrows
 //
@@ -17,10 +17,12 @@
 //  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 //  Boston, MA 02111-1307, USA.
 
-#ifndef MATCHERS_H
-#define MATCHERS_H
+#ifndef PARSE_H
+#define PARSE_H
 
 #include <cwidget/generic/util/ref_ptr.h>
+
+/** \file parse.h */
 
 /** \brief Code to parse search patterns. */
 
@@ -30,7 +32,33 @@ namespace aptitude
   {
     class pattern;
 
-
+    /** \brief Parse a string region as a search pattern.
+     *
+     *  Parses [start, end), treating strings from terminators as
+     *  ending the parse if they are encountered at the top syntactic
+     *  level.
+     *
+     *  \param start   The beginning of the range to parse.
+     *                 Will be moved past the parsed expression.
+     *  \param end     The end of the range to parse.
+     *  \param terminators  A list of strings whose (unescaped)
+     *                      presence at the top syntactic level
+     *                      signals the end of a search pattern.
+     *  \param search_descriptions  If \b true, unadorned strings
+     *                              will generate ?description
+     *                              terms in addition to ?name
+     *                              terms.
+     *  \param flag_errors  If \b true, apt errors (in _error)
+     *                      will be generated for any parsing
+     *                      errors that are encountered.
+     *  \param require_full_parse  If \b true, an error will be
+     *                             signalled if part of the region
+     *                             is left after parsing (i.e.,
+     *                             if start != end).
+     *
+     *  \return  The parsed expression, or NULL if an
+     *  error occurred or if the input range was empty.
+     */
     cwidget::util::ref_ptr<pattern>
       parse(std::string::const_iterator &start,
 	    const std::string::const_iterator &end,
@@ -39,6 +67,31 @@ namespace aptitude
 	    bool flag_errors,
 	    bool require_full_parse);
 
+
+    /** \brief Parse a string as a search pattern.
+     *
+     *  Parses the given string, treating strings from terminators as
+     *  ending the parse if they are encountered at the top syntactic
+     *  level.
+     *
+     *  \param s       The string to parse.
+     *  \param terminators  A list of strings whose (unescaped)
+     *                      presence at the top syntactic level
+     *                      signals the end of a search pattern.
+     *  \param search_descriptions  If \b true, unadorned strings
+     *                              will generate ?description
+     *                              terms in addition to ?name
+     *                              terms.
+     *  \param flag_errors  If \b true, apt errors (in _error)
+     *                      will be generated for any parsing
+     *                      errors that are encountered.
+     *  \param require_full_parse  If \b true, an error will be
+     *                             signalled if part of the string
+     *                             is left after parsing.
+     *
+     *  \return  The parsed expression, or NULL if an
+     *  error occurred or if the input string was empty.
+     */
     inline cwidget::util::ref_ptr<pattern>
       parse(const std::string &s,
 	    const std::vector<const char *> &terminators,
@@ -54,6 +107,7 @@ namespace aptitude
 		   require_full_parse);
     }
 
+    /** \brief Parse a string as a search pattern. */
     inline cwidget::util::ref_ptr<pattern>
       parse(const std::string &s,
 	    bool search_descriptions,
@@ -66,6 +120,17 @@ namespace aptitude
 		   require_full_parse);
     }
 
+    /** \brief Parse a string as a search pattern.
+     *
+     *  Bare strings only match names, errors will be flagged by
+     *  generating apt errors, and it is an error if part of the input
+     *  string is left over after parsing.
+     *
+     *  \param s       The string to parse.
+     *
+     *  \return  The parsed expression, or NULL if an
+     *  error occurred or if the input string was empty.
+     */
     inline cwidget::util::ref_ptr<pattern>
       parse(const std::string &s)
     {
