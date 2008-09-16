a38cd164c42111d093fc1327b6d7e88e53bbb7eb
Daniel Burrows <dburrows@debian.org>
2008-09-15 19:55:03 -0700

    Redesign how matches are represented to handle version information cleanly. I think this is a reasonable final version of the match structures. It uses a two-level tree structure, where the outer level represents the parts of the pattern that don't match particular versions of particular packages, and the inner level describes the actual versions that were matched and which patterns matched them.
    Still to do: the code to actually generate these things needs to be
    written; the main open question here is how completely I'll fill in the
    match structure.  I might try some sort of three-level match if I can
    find an efficient implementation: at the lowest level, it just returns
    a bool ("yes it matched" vs "no it didn't"); at the next level, it
    returns some match information but with short-circuiting (so some
    versions that could have matched will be left out), and at the highest
    level it will find every version that would have matched; e.g., all the
    clauses of an "or" will always be examined, and in "any" mode all the
    matchables in the current pool will be examined (even if a match is
    found).
    
    I still wonder what to do about the children of a "not", but for now I
    just won't describe them.

--- src/generic/apt/matching/match.h
+++ src/generic/apt/matching/match.h
@@ -40,6 +40,203 @@ namespace aptitude
 {
   namespace matching
   {
+    /** \brief Represents the atomic values that are selected by search
+     *  patterns.
+     *
+     *  Each matchable object is either a virtual package, or a
+     *  particular version of a package.
+     */
+    class matchable
+    {
+      // The package and version; if the version is invalid, this
+      // represents a virtual package.
+      pkgCache::Package *pkg;
+      pkgCache::Version *ver;
+
+    public:
+      matchable(pkgCache::Package *_pkg,
+		pkgCache::Version *_ver)
+	: pkg(_pkg), ver(_ver)
+      {
+      }
+
+      pkgCache::Package *get_pkg() const { return pkg; }
+      pkgCache::Version *get_ver() const { return ver; }
+
+      pkgCache::PkgIterator get_package_iterator(pkgCache &owner) const
+      {
+	return pkgCache::PkgIterator(owner, pkg);
+      }
+
+      bool get_has_version() const { return ver != NULL; }
+
+      pkgCache::VerIterator get_version_iterator(pkgCache &owner) const
+      {
+	eassert(ver != NULL);
+
+	return pkgCache::VerIterator(owner, ver);
+      }
+
+      /** \brief Compare two matchables.
+       *
+       *  Matchables are ordered first by their package, then by their
+       *  version (if any).  Version ordering is arbitrary but
+       *  well-defined.
+       */
+      bool operator<(const matchable &other) const
+      {
+	if(pkg < other.pkg)
+	  return true;
+	else if(pkg > other.pkg)
+	  return false;
+	else if(ver < other.ver)
+	  return true;
+	else if(ver > other.ver)
+	  return false;
+	else
+	  return false;
+      }
+    };
+
+    /** \brief A match for a node that is "atomic" at the level of
+     *  matches against a particular pool of possibilities.
+     *
+     *  Representing matches is tricky because we have to handle
+     *  version information in a sensible way.  Each pattern could
+     *  match multiple versions, and this information has to be
+     *  represented for the user of the match to make sense of it
+     *  since, e.g., every version can have a different Maintainer
+     *  field.
+     *
+     *  We can divide term types into two categories: some terms
+     *  return true by testing a condition against the packages in the
+     *  incoming version pool, while other terms simply delegate to
+     *  their sub-parts.  We call the second category \e structural
+     *  terms.  Note that terms such as ?depends, which \e ignore the
+     *  incoming pool and begin a new match against a new pool, are in
+     *  the second category, not the first category.  The structural
+     *  terms are:
+     *
+     *   - ?and
+     *   - ?or
+     *   - ?not
+     *   - ?any
+     *   - ?all
+     *   - ?widen
+     *   - ?narrow
+     *
+     *  To represent a match, we use a heterogeneous tree.  The inner
+     *  nodes represent the structural matchers that are encountered
+     *  at the root of the tree, and the leaves represent the
+     *  structually-atomic matchers (again, these may be matchers with
+     *  sub-expressions, but their sub-expression represents a
+     *  completely new search).
+     *
+     *  Structural matches either represent an internal node (i.e., a
+     *  match using one of the above patterns) or an atomic node (a
+     *  match to one of the other patterns).
+     */
+    class structural_match : public util::refcounted_base
+    {
+    public:
+      /** \brief The type of this structural match node. */
+      enum type
+	{
+	  /** \brief Am internal match node; it has a number
+	   *  of sub-matches (get_sub_matches()).
+	   */
+	  internal,
+	  /** \brief An atomic match node; it has an attached list of
+	   *  pairs containing a matchable object and a match object
+	   *  describing how it was matched.
+	   */
+	  atomic
+	};
+
+    private:
+      type tp;
+
+      // The corresponding pattern node.
+      cwidget::util::ref_ptr<pattern> pattern;
+
+      // The list of structural sub-matches, if any.  Each corresponds
+      // to a different sub-pattern (e.g., the list of immediate
+      // children of a ?and node).
+      std::vector<cwidget::util::ref_ptr<structural_match> > sub_matches;
+
+      // The atomic match values, if any.  Each corresponds to how a
+      // single matchable object satisfied the search.
+      std::vector<std::pair<matchable, cwidget::util::ref_ptr<match> > > atomic_matches;
+
+      template<typename StructuralMatchIter,
+	       typename AtomicMatchIter>
+      structural_match(type _tp,
+		       const cwidget::util::ref_ptr<pattern> &_pattern,
+		       StructuralMatchIter _sub_matches_begin,
+		       StructuralMatchIter _sub_matches_end,
+		       AtomicMatchIter _atomic_matches_begin,
+		       AtomicMatchIter _atomic_matches_end)
+	: tp(_tp), pattern(_pattern),
+	  sub_matches(_sub_matches_begin, _sub_matches_end),
+	  atomic_matches(_atomic_matches_begin, _atomic_matches_end)
+      {
+      }
+
+    public:
+      /** \brief Create a new inner structural match node.
+       *
+       *  \typeparam StructuralMatchIter An input iterator returning
+       *                                 values of type
+       *                                 cwidget::util::ref_ptr<structural_match>.
+       *
+       *  \param pattern   The corresponding match pattern.
+       *
+       *  \param sub_matches_begin   The beginning of the range
+       *                             of sub-nodes.
+       *
+       *  \param sub_matches_end     The end of the range of
+       *                             sub-nodes.
+       */
+      template<typename StructuralMatchIter>
+      static cwidget::util::ref_ptr<structural_match> make_branch(const cwidget::util::ref_ptr<pattern> &pattern,
+						   StructuralMatchIter sub_matches_begin,
+						   StructuralMatchIter sub_matches_end)
+      {
+	return new structural_match(internal,
+				    pattern,
+				    sub_matches_begin,
+				    sub_matches_end,
+				    (std::pair<matchable, cwidget::util::ref_ptr<match> > *)0,
+				    (std::pair<matchable, cwidget::util::ref_ptr<match> > *)0);
+      }
+
+      /** \brief Create a new leaf structural match node.
+       *
+       *  \typeparam AtomicMatchIter An input iterator returning
+       *                             values of type
+       *                             std::pair<matchable, cwidget::util::ref_ptr<match> >.
+       *
+       *  \param pattern   The corresponding match pattern.
+       *
+       *  \param atomic_matches_begin   The beginning of the
+       *                                range of atomic matches.
+       *  \param atomic_matches_end     The end of the range
+       *                                of atomic matches.
+       */
+      template<typename AtomicMatchIter>
+      static cwidget::util::ref_ptr<structural_match> make_leaf(const cwidget::util::ref_ptr<pattern> &pattern,
+								AtomicMatchIter atomic_matches_begin,
+								AtomicMatchIter atomic_matches_end)
+      {
+	return new structural_match(atomic,
+				    pattern,
+				    (cwidget::util::ref_ptr<structural_match> *)0,
+				    (cwidget::util::ref_ptr<structural_match> *)0,
+				    atomic_matches_begin,
+				    atomic_matches_end);
+      }
+    };
+
     /** \brief Represents information about how a package was matched.
      *
      *  This only represents the information that can't be derived
@@ -54,6 +251,12 @@ namespace aptitude
     class match : public util::refcounted_base
     {
     public:
+      class dependency_match_info
+      {
+	pkgCache::DepIterator dep;
+	
+      };
+
       /** \brief The type of a match. */
       enum type
 	{
@@ -73,22 +276,11 @@ namespace aptitude
 
 	  /** \brief A match that was made via a dependency.
 	   *
-	   *  The attached information consists of the package /
-	   *  version that the sub-query matched against, the
-	   *  dependency the match was made through, and a list of the
-	   *  matches that were made through the dependency.
+	   *  The attached information is a structural matcher
+	   *  representing how the sub-expression matched, along with
+	   *  the dependency that was followed.
 	   */
-	  dependency,
-
-	  /** \brief A match that was made against a list of sub-patterns.
-	   *
-	   *  The semantics of the sub-patterns are determined by the
-	   *  pattern corresponding to this match: for instance, an
-	   *  "?and" term requires all its sub-terms to match, whereas
-	   *  a ?widen term matches if its sub-terms match any version
-	   *  of the incoming package.
-	   */
-	  sub_pattern_list
+	  dependency
 	};
 
       /** \brief Represents a match of a regular expression against a string.
@@ -126,27 +318,26 @@ namespace aptitude
       // The dependency that was followed, if any.
       pkgCache::DepIterator dep;
 
-      // The package version that produced the match.
-      pkgCache::VerIterator ver;
-
       // The string regions matched by the regular expression, if any.
       std::vector<regexp_match> regexp_matches;
 
-      // Proofs about sub-matches, if any.
-      std::vector<cwidget::util::ref_ptr<match> > sub_matches;
+      // The sub-match, if any.
+      cwidget::util::ref_ptr<structural_match> sub_match;
 
       // NB: maybe I should avoid 
       template<typename RegexpMatchIter,
 	       typename SubMatchIter>
       match(type _tp,
 	    const cwidget::util::ref_ptr<pattern> &_p,
+	    const cwidget::util::ref_ptr<structural_match> &_sub_match,
 	    pkgCache::DepIterator _dep,
-	    pkgCache::VerIterator _ver,
 	    RegexpMatchIter regexp_matches_begin,
 	    RegexpMatchIter regexp_matches_end,
 	    SubMatchIter sub_matches_begin,
 	    SubMatchIter sub_matches_end)
-	: tp(_tp), p(_p), dep(_dep), ver(_ver),
+	: tp(_tp), p(_p),
+	  match_targets(_match_targets),
+	  dep(_dep),
 	  regexp_matches(regexp_matches_begin,
 			 regexp_matches_end),
 	  sub_matches(sub_matches_begin,
@@ -162,8 +353,8 @@ namespace aptitude
       static cwidget::util::ref_ptr<match> make_atomic(const cwidget::util::ref_ptr<pattern> &p)
       {
 	return new match(atomic, p,
+			 cwidget::util::ref_ptr<structural_match>(),
 			 pkgCache::DepIterator(),
-			 pkgCache::VerIterator(),
 			 (regexp_match *)0, (regexp_match *)0,
 			 (cwidget::util::ref_ptr<match> *)0, (cwidget::util::ref_ptr<match> *)0);
       }
@@ -184,8 +375,8 @@ namespace aptitude
 						       RegexpMatchIter regexp_matches_end)
       {
 	return new match(regexp, p,
+			 cwidget::util::ref_ptr<structural_match>(),
 			 pkgCache::DepIterator(),
-			 pkgCache::VerIterator(),
 			 regexp_matches_begin, regexp_matches_end,
 			 (cwidget::util::ref_ptr<match> *)0,
 			 (cwidget::util::ref_ptr<match> *)0);
@@ -195,7 +386,6 @@ namespace aptitude
        *
        *  \param p   The pattern that produced this match.
        *  \param dep The dependency that was followed.
-       *  \param ver The version that was matched.
        *  \param sub_matches_begin   The beginning of the
        *                             range of sub-matches.
        *  \param sub_matches_end     The end of the range
@@ -203,12 +393,14 @@ namespace aptitude
        */
       template<typename SubMatchIter>
       static cwidget::util::ref_ptr<match> make_dependency(const cwidget::util::ref_ptr<pattern> &p,
+							   const cwidget::util::ref_ptr<structural_match> &m,
 							   const pkgCache::DepIterator &dep,
 							   const pkgCache::VerIterator &ver,
 							   SubMatchIter sub_matches_begin,
 							   SubMatchIter sub_matches_end)
       {
 	return new match(dependency, p,
+			 m,
 			 dep,
 			 (regexp_match *)0, 0,
 			 sub_matches_begin,
@@ -231,8 +423,8 @@ namespace aptitude
 								 SubMatchIter sub_matches_end)
       {
 	return new match(sub_pattern_list, p,
+			 cwidget::util::ref_ptr<structural_match>(),
 			 pkgCache::DepIterator(),
-			 pkgCache::VerIterator(),
 			 (regexp_match *)0, 0,
 			 sub_matches_begin,
 			 sub_matches_end);
@@ -247,6 +439,14 @@ namespace aptitude
       const cwidget::util::ref_ptr<pattern> &get_pattern() const
       {
 	return p;
+      }
+
+      /** \brief Retrieve the objects that were the target
+       *  of this match.
+       */
+      const cwidget::util::ref_ptr<matchable_list> &get_match_targets() const
+      {
+	return match_targets;
       }
 
       /** \brief For regular expression matches, retrieve the list of
@@ -267,16 +467,6 @@ namespace aptitude
 	eassert(tp == dependency);
 
 	return dep;
-      }
-
-      /** \brief For dependency matches, retrieve the package version
-       *  that produced the match.
-       */
-      const pkgCache::VerIterator &get_ver() const
-      {
-	eassert(tp == dependency);
-
-	return ver;
       }
 
       /** \brief For dependency matches or sub-matches, return the
