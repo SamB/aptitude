dce41a640f79ca388b6e05e2a468f62235dc5a61
Daniel Burrows <dburrows@debian.org>
2008-09-24 18:57:19 -0700

    Implement ?installed.

--- src/generic/apt/matching/match.cc
+++ src/generic/apt/matching/match.cc
@@ -541,7 +541,11 @@ namespace aptitude
 	    break;
 
 	  case pattern::installed:
-	    return NULL;
+	    if(target.get_has_version() &&
+	       target.get_version_iterator(cache) == target.get_package_iterator(cache).CurrentVer())
+	      return match::make_atomic(p);
+	    else
+	      return NULL;
 	    break;
 
 	  case pattern::maintainer:
