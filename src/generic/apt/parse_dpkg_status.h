01b02063c7fe0c947e513ceba2e897d4eab78a4e
Daniel Burrows <dburrows@debian.org>
2008-11-16 12:43:57 -0800

    Finish fleshing out the dpkg status parsing interface, and fill in the implementation. The implementation is mostly based on reverse-engineering the message syntax from the synaptic source code, since I can't find up-to-date documentation on it.  The one hole in the parser is that it will do the wrong thing if a conffile filename contains a single quote, but so will synaptic and I don't know how dpkg escapes the inner quote (backslashes maybe?).

--- src/generic/apt/parse_dpkg_status.h
+++ src/generic/apt/parse_dpkg_status.h
@@ -23,10 +23,27 @@
 #include <deque>
 #include <string>
 
+#include <ostream>
+
+#include <cwidget/generic/util/exception.h>
+
 namespace aptitude
 {
   namespace apt
   {
+    class DpkgStatusException : public cwidget::util::Exception
+    {
+      std::string msg;
+
+    public:
+      DpkgStatusException(const std::string &_msg)
+	: msg(_msg)
+      {
+      }
+
+      std::string errmsg() const { return msg; }
+    };
+
     /** \brief Represents a status message from dpkg. */
     class dpkg_status_message
     {
@@ -55,12 +72,14 @@ namespace aptitude
       std::string existing_filename;
       std::string new_filename;
       double percent;
+      std::string package;
       std::string text;
 
       dpkg_status_message(type _tp,
 			  const std::string &_existing_filename,
 			  const std::string &_new_filename,
 			  double _percent,
+			  const std::string &_package,
 			  const std::string &_text)
 	: tp(_tp),
 	  existing_filename(_existing_filename),
@@ -72,9 +91,10 @@ namespace aptitude
 
     public:
       static dpkg_status_message make_error(double percent,
+					    const std::string &package,
 					    const std::string &text)
       {
-	return dpkg_status_message(error, std::string(), std::string(), percent, text);
+	return dpkg_status_message(error, std::string(), std::string(), percent, package, text);
       }
 
       static dpkg_status_message make_conffile(const std::string &existing_filename,
@@ -82,13 +102,44 @@ namespace aptitude
 					       double percent,
 					       const std::string &text)
       {
-	return dpkg_status_message(conffile, existing_filename, new_filename, percent, text);
+	return dpkg_status_message(conffile, existing_filename, new_filename, percent, std::string(), text);
       }
 
       static dpkg_status_message make_status(double percent,
+					     const std::string &package,
 					     const std::string &text)
       {
-	return dpkg_status_message(status, std::string(), std::string(), percent, text);
+	return dpkg_status_message(status, std::string(), std::string(), percent, package, text);
+      }
+
+      bool operator==(const dpkg_status_message &other) const
+      {
+	if(tp != other.tp)
+	  return false;
+
+	switch(tp)
+	  {
+	  case error:
+	    return
+	      percent == other.percent &&
+	      package == other.package &&
+	      text == other.text;
+
+	  case conffile:
+	    return
+	      existing_filename == other.existing_filename &&
+	      new_filename == other.new_filename &&
+	      percent == other.percent &&
+	      text == other.text;
+
+	  case status:
+	    return
+	      percent == other.percent &&
+	      package == other.package &&
+	      text == other.text;
+	  }
+
+	return false;
       }
 
       /** \brief Parse a status message from the given character buffer.
@@ -101,10 +152,37 @@ namespace aptitude
       /** \brief Get the type of message that this is. */
       type get_type() const { return tp; }
 
-      /** \brief For conffile messages, get the name of the file.
-       *  with this
+      /** \brief Get the name of the package associated with this message.
+       *
+       *  Not meaningful for conffile messages.
        */
+      const std::string &get_package() const { return package; }
+
+      /** \brief Get the progress percentage associated with this message. */
+      double get_percent() const { return percent; }
+
+      /** \brief Get the message text associated with this message.
+       *
+       *  For errors, this is the error message; for conffile
+       *  messages, this is the name of the file as reported in the
+       *  dpkg message; for other status updates, this is a
+       *  description of the current status.
+       */
+      const std::string &get_text() const { return text; }
+
+      /** \brief For conffile messages, get the name of the currently
+       *  installed version of the conffile.
+       */
+      const std::string &get_existing_filename() const { return existing_filename; }
+
+      /** \brief For conffile messages, get the name of the new version
+       *  of the conffile (the one that was unpacked from the .deb).
+       */
+      const std::string &get_new_filename() const { return new_filename; }
     };
+
+    // Dump a status message for debugging purposes.
+    std::ostream &operator<<(std::ostream &out, const dpkg_status_message &mgs);
 
     /** \brief A parser for the dpkg status pipe. */
     class dpkg_status_parser
