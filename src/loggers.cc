98fec9cfb3adb6f93c409dcb5201b480085bfd87
Daniel Burrows <dburrows@debian.org>
2009-01-30 22:51:13 -0800

    Add a file that will serve as a central repository for all the loggers used in aptitude.

---
+++ src/loggers.cc
@@ -0,0 +1,35 @@
+/** \file loggers.cc */
+
+//   Copyright (C) 2009 Daniel Burrows
+
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of
+//   the License, or (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program; see the file COPYING.  If not, write to
+//   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+//   Boston, MA 02111-1307, USA.
+
+#include "loggers.h"
+
+using namespace log4cxx;
+
+namespace aptitude
+{
+  namespace
+  {
+    LoggerPtr loggerAptitudeResolver(Logger::getLogger("aptitude.resolver"));
+  }
+
+  LoggerPtr Loggers::getAptitudeResolver()
+  {
+    return loggerAptitudeResolver;
+  }
+}
