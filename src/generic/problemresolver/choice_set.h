9d343a28bc2ecb9eec6342e11b433ae4e59ca3f5
Daniel Burrows <dburrows@debian.org>
2009-03-01 16:09:31 -0800

    Write a class that encapsulates the business of storing a set of choices and eliminating overlaps, and write a test case for it.

---
+++ src/generic/problemresolver/choice_set.h
@@ -0,0 +1,280 @@
+/** \file choice_set.h */ // -*-c++-*-
+
+//   Copyright (C) 2009 Daniel Burrows
+//
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of
+//   the License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; see the file COPYING.  If not, write to
+//   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+//   Boston, MA 02111-1307, USA.
+
+#ifndef CHOICE_SET_H
+#define CHOICE_SET_H
+
+#include "choice.h"
+#include <generic/util/immset.h>
+
+#include <iostream>
+
+template<typename PackageUniverse> class generic_choice_set;
+template<typename PackageUniverse>
+std::ostream &operator<<(std::ostream &out, const generic_choice_set<PackageUniverse> &choices);
+
+/** \brief Stores a set of choices, with the ability to quickly answer
+ *  questions about containment and to prune choices that are
+ *  redundant.
+ *
+ *  This object requires that the choices form a coherent installation
+ *  set.  In particular, two install_version choices that install
+ *  different versions for the same package may not coexist.
+ *  Furthermore, if two choices install the same version, one of the
+ *  choices must contain the other.  The choice sets generated during
+ *  dependency resolution all have these properties "automatically".
+ *  (these restrictions could be lifted, at the cost of making this
+ *  code a bit more complex and possibly slower; you'd need to
+ *  introduce a map from dependencies to choices and store one of
+ *  those for each version)
+ *
+ *  One set of choices contains another set if each choice in the
+ *  first set is contained by a choice in the second set.  This class
+ *  uses its knowledge of the structure of choices to accelerate that
+ *  test.
+ */
+template<typename PackageUniverse>
+class generic_choice_set
+{
+public:
+  typedef generic_choice<PackageUniverse> choice;
+
+private:
+  typedef typename PackageUniverse::package package;
+  typedef typename PackageUniverse::version version;
+  typedef typename PackageUniverse::dep dep;
+
+  // Note: no iterators on this structure because they'd be a pain to
+  // write and they should never be used (use for_each instead, it's a
+  // lot more efficient).
+
+  // Stores the install-this-version choices made in this set,
+  // organized by package.  Needed to accelerate the containment test.
+  imm::map<package, choice> install_version_choices;
+
+  // Stores the dependencies that are broken by this choice set.  We
+  // could just store dependencies instead; I don't know whether that
+  // would be more efficient or not.
+  imm::set<choice> not_install_version_choices;
+
+  friend std::ostream &operator<< <PackageUniverse>(std::ostream &out, const generic_choice_set<PackageUniverse> &choices);
+
+  // Used to apply a for_each on choices to each element of the
+  // install_version_choices set.
+  template<typename F>
+  struct for_each_choice_pair
+  {
+    const F &f;
+
+    for_each_choice_pair(const F &_f) : f(_f)
+    {
+    }
+
+    bool operator()(const std::pair<package, choice> &p) const
+    {
+      f(p.second);
+      return true;
+    }
+  };
+
+  // Used by operator<< -- could be declared outside this object, but
+  // that would pollute the namespace.
+  struct show_choice
+  {
+    mutable bool first;
+    std::ostream &out;
+
+    show_choice(std::ostream &_out, bool first)
+      : first(true), out(_out)
+    {
+    }
+
+    bool operator()(const choice &c) const
+    {
+      if(first)
+	first = false;
+      else
+	out << ", ";
+      out << c;
+
+      return true;
+    }
+  };
+
+  struct insert_choice
+  {
+    generic_choice_set &parent;
+
+    insert_choice(generic_choice_set &_parent)
+      : parent(_parent)
+    {
+    }
+
+    bool operator()(const choice &c) const
+    {
+      switch(c.get_type())
+	{
+	case choice::install_version:
+	  // Check whether this "hits" an existing choice.
+	  {
+	    const package p(c.get_ver().get_package());
+
+	    typename imm::map<package, choice>::node n =
+	      parent.install_version_choices.lookup(p);
+
+	    if(!n.isValid())
+	      parent.install_version_choices.put(p, c);
+	    else
+	      {
+		std::pair<package, choice> existing_choice_pair(n.getVal());
+		choice &existing_choice(existing_choice_pair.second);
+
+		if(existing_choice.contains(c))
+		  ; // The new choice adds no information.
+		else if(c.contains(existing_choice))
+		  // Override the existing choice with the new one,
+		  // which is more general.
+		  parent.install_version_choices.put(p, c);
+		else
+		  eassert(!"Internal error: attempted to add conflicting choices to the same set.");
+	      }
+	  }
+	  break;
+
+	default:
+	  parent.not_install_version_choices.insert(c);
+	  break;
+	}
+
+      return true;
+    }
+  };
+
+  // Used to check the install_version_choices map for supermap-ness
+  // when testing set containment, and to check the
+  // not_install_version_choices map for superset-ness.
+  struct choice_contains
+  {
+    choice_contains()
+    {
+    }
+
+    // The order of "contains" is reversed because of how the
+    // predicate is invoked (p1 and c1 are from the set that should be
+    // a subset; see immset.h).  Arguably that was a bad choice on my
+    // part when I was designing the immset code, but I'm pretty much
+    // stuck with it now.
+    bool operator()(const std::pair<package, choice> &p1,
+		    const std::pair<package, choice> &p2) const
+    {
+      return p2.second.contains(p1.second);
+    }
+
+    bool operator()(const choice &c1, const choice &c2) const
+    {
+      return c2.contains(c1);
+    }
+  };
+
+public:
+  generic_choice_set()
+  {
+  }
+
+  generic_choice_set(const imm::set<choice> &choices)
+  {
+    choices.for_each(insert_choice(*this));
+  }
+
+  void insert(const choice &c)
+  {
+    insert_choice(*this)(c);
+  }
+
+  bool contains(const choice &c) const
+  {
+    switch(c.get_type())
+      {
+      case choice::install_version:
+	{
+	  typename imm::map<package, choice>::node n =
+	    install_version_choices.lookup(c.get_ver().get_package());
+	  if(!n.isValid())
+	    return false;
+	  else
+	    {
+	      std::pair<package, choice> existing_choice_pair(n.getVal());
+	      choice &existing_choice(existing_choice_pair.second);
+	      return existing_choice.contains(c);
+	    }
+	}
+
+      default:
+	return not_install_version_choices.contains(c);
+      }
+  }
+
+  typedef unsigned int size_type;
+  size_type size() const
+  {
+    return install_version_choices.size() + not_install_version_choices.size();
+  }
+
+  bool operator==(const generic_choice_set &other) const
+  {
+    return
+      install_version_choices == other.install_version_choices &&
+      not_install_version_choices == other.not_install_version_choices;
+  }
+
+  bool operator!=(const generic_choice_set &other) const
+  {
+    return !(*this == other);
+  }
+
+  /** \brief Check whether each entry in the other set is contained by
+   *  an entry in this set.
+   */
+  bool contains(const generic_choice_set &other) const
+  {
+    const choice_contains f;
+
+    return
+      install_version_choices.is_supermap_of_under(other.install_version_choices, f) &&
+      not_install_version_choices.contains(other.not_install_version_choices, f);
+  }
+
+  /** \brief Apply a function object to each element in this set. */
+  template<typename F>
+  void for_each(const F &f) const
+  {
+    install_version_choices.for_each(for_each_choice_pair<F>(f));
+    not_install_version_choices.for_each(f);
+  }
+};
+
+template<typename PackageUniverse>
+std::ostream &operator<<(std::ostream &out, const generic_choice_set<PackageUniverse> &choices)
+{
+  typename generic_choice_set<PackageUniverse>::show_choice f(out, true);
+  choices.for_each(f);
+  return out;
+}
+
+#endif // CHOICE_SET_H
---
+++ tests/test_choice_set.cc
@@ -0,0 +1,329 @@
+// test_choice_set.cc
+//
+//   Copyright (C) 2009 Daniel Burrows
+//
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of
+//   the License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; see the file COPYING.  If not, write to
+//   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+//   Boston, MA 02111-1307, USA.
+
+#include <generic/problemresolver/choice_set.h>
+#include <generic/problemresolver/dummy_universe.h>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace
+{
+  const char *dummy_universe_1 = "\
+UNIVERSE [			  \
+  PACKAGE a < v1 v2 v3 > v1	  \
+  PACKAGE b < v1 v2 v3 > v1	  \
+  PACKAGE c < v1 v2 v3 > v1	  \
+				  \
+  DEP a v1 -> < b v2 >		  \
+  DEP b v2 -> < c v2 >		  \
+				  \
+  DEP a v2 -> < >		  \
+  DEP a v3 -> < >		  \
+]";
+}
+
+class Choice_Set_Test : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE(Choice_Set_Test);
+
+  CPPUNIT_TEST(testInsert);
+  CPPUNIT_TEST(testContainsChoice);
+  CPPUNIT_TEST(testContainsChoiceSet);
+  // No test for for_each(), because it's tested in testInsert (at
+  // each step, we learn what's really in the set using for_each())
+
+  CPPUNIT_TEST_SUITE_END();
+
+
+  typedef dummy_universe_ref::package package;
+  typedef dummy_universe_ref::version version;
+  typedef dummy_universe_ref::dep dep;
+  typedef generic_choice<dummy_universe_ref> choice;
+  typedef generic_choice_set<dummy_universe_ref> choice_set;
+
+  static dummy_universe_ref parseUniverse(const std::string &s)
+  {
+    std::istringstream in(s);
+
+    return parse_universe(in);
+  }
+
+  struct contents_extractor
+  {
+    imm::set<choice> &rval;
+
+    contents_extractor(imm::set<choice> &_rval)
+      : rval(_rval)
+    {
+    }
+
+    bool operator()(const choice &c) const
+    {
+      CPPUNIT_ASSERT(!rval.contains(c));
+
+      rval.insert(c);
+
+      return true;
+    }
+  };
+
+  static imm::set<choice> get_contents(const choice_set &s)
+  {
+    imm::set<choice> rval;
+    s.for_each(contents_extractor(rval));
+    return rval;
+  }
+
+public:
+  void testInsert()
+  {
+    dummy_universe_ref u(parseUniverse(dummy_universe_1));
+
+    package a(u.find_package("a"));
+    package b(u.find_package("b"));
+    package c(u.find_package("c"));
+
+    version av1(a.version_from_name("v1"));
+    version av2(a.version_from_name("v2"));
+    version av3(a.version_from_name("v3"));
+
+    version bv1(b.version_from_name("v1"));
+    version bv2(b.version_from_name("v2"));
+    version bv3(b.version_from_name("v3"));
+
+    version cv1(c.version_from_name("v1"));
+    version cv2(c.version_from_name("v2"));
+    version cv3(c.version_from_name("v3"));
+
+    dep av1d1(*av1.deps_begin());
+    dep bv2d1(*bv2.deps_begin());
+    dep av2d1(*av2.deps_begin());
+    dep av3d1(*av3.deps_begin());
+
+    choice_set s;
+    imm::set<choice> expected;
+
+    CPPUNIT_ASSERT_EQUAL(expected.size(), s.size());
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(s));
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(choice_set(expected)));
+    CPPUNIT_ASSERT_EQUAL(s, choice_set(expected));
+
+    const choice c1(choice::make_install_version(av2));
+    s.insert(c1);
+    expected.insert(c1);
+
+    CPPUNIT_ASSERT_EQUAL(expected.size(), s.size());
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(s));
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(choice_set(expected)));
+    CPPUNIT_ASSERT_EQUAL(s, choice_set(expected));
+
+    const choice c2(choice::make_install_version_from_dep_source(av2, av3d1));
+    s.insert(c2);
+
+    CPPUNIT_ASSERT_EQUAL(expected.size(), s.size());
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(s));
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(choice_set(expected)));
+    CPPUNIT_ASSERT_EQUAL(s, choice_set(expected));
+
+    const choice c3(choice::make_break_soft_dep(av2d1));
+    s.insert(c3);
+    expected.insert(c3);
+    CPPUNIT_ASSERT_EQUAL(expected.size(), s.size());
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(s));
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(choice_set(expected)));
+    CPPUNIT_ASSERT_EQUAL(s, choice_set(expected));
+
+    const choice c4(choice::make_install_version_from_dep_source(bv1, bv2d1));
+    s.insert(c4);
+    expected.insert(c4);
+
+    CPPUNIT_ASSERT_EQUAL(expected.size(), s.size());
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(s));
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(choice_set(expected)));
+    CPPUNIT_ASSERT_EQUAL(s, choice_set(expected));
+
+    const choice c5(choice::make_install_version(bv1));
+    s.insert(c5);
+    expected.insert(c5);
+    expected.erase(c4);
+
+    CPPUNIT_ASSERT_EQUAL(expected.size(), s.size());
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(s));
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(choice_set(expected)));
+    CPPUNIT_ASSERT_EQUAL(s, choice_set(expected));
+
+
+    const choice c6(choice::make_break_soft_dep(av3d1));
+    s.insert(c6);
+    expected.insert(c6);
+    CPPUNIT_ASSERT_EQUAL(expected.size(), s.size());
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(s));
+    CPPUNIT_ASSERT_EQUAL(expected, get_contents(choice_set(expected)));
+    CPPUNIT_ASSERT_EQUAL(s, choice_set(expected));
+  }
+
+  void testContainsChoice()
+  {
+    dummy_universe_ref u(parseUniverse(dummy_universe_1));
+
+    package a(u.find_package("a"));
+    package b(u.find_package("b"));
+    package c(u.find_package("c"));
+
+    version av1(a.version_from_name("v1"));
+    version av2(a.version_from_name("v2"));
+    version av3(a.version_from_name("v3"));
+
+    version bv1(b.version_from_name("v1"));
+    version bv2(b.version_from_name("v2"));
+    version bv3(b.version_from_name("v3"));
+
+    version cv1(c.version_from_name("v1"));
+    version cv2(c.version_from_name("v2"));
+    version cv3(c.version_from_name("v3"));
+
+    dep av1d1(*av1.deps_begin());
+    dep bv2d1(*bv2.deps_begin());
+    dep av2d1(*av2.deps_begin());
+    dep av3d1(*av3.deps_begin());
+
+    choice_set s;
+    s.insert(choice::make_install_version_from_dep_source(av1, av3d1));
+    s.insert(choice::make_install_version(cv3));
+    s.insert(choice::make_install_version(bv1));
+    s.insert(choice::make_break_soft_dep(av2d1));
+
+    CPPUNIT_ASSERT(s.contains(choice::make_install_version_from_dep_source(av1, av3d1)));
+    CPPUNIT_ASSERT(!s.contains(choice::make_install_version_from_dep_source(av1, av2d1)));
+    CPPUNIT_ASSERT(!s.contains(choice::make_install_version(av1)));
+
+    CPPUNIT_ASSERT(s.contains(choice::make_install_version(cv3)));
+    CPPUNIT_ASSERT(!s.contains(choice::make_install_version(cv2)));
+    CPPUNIT_ASSERT(s.contains(choice::make_install_version_from_dep_source(cv3, av2d1)));
+
+    CPPUNIT_ASSERT(s.contains(choice::make_install_version(bv1)));
+    CPPUNIT_ASSERT(!s.contains(choice::make_install_version(bv2)));
+    CPPUNIT_ASSERT(s.contains(choice::make_install_version_from_dep_source(bv1, bv2d1)));
+
+    CPPUNIT_ASSERT(s.contains(choice::make_break_soft_dep(av2d1)));
+    CPPUNIT_ASSERT(!s.contains(choice::make_break_soft_dep(av3d1)));
+  }
+
+  void testContainsChoiceSet()
+  {
+    dummy_universe_ref u(parseUniverse(dummy_universe_1));
+
+    package a(u.find_package("a"));
+    package b(u.find_package("b"));
+    package c(u.find_package("c"));
+
+    version av1(a.version_from_name("v1"));
+    version av2(a.version_from_name("v2"));
+    version av3(a.version_from_name("v3"));
+
+    version bv1(b.version_from_name("v1"));
+    version bv2(b.version_from_name("v2"));
+    version bv3(b.version_from_name("v3"));
+
+    version cv1(c.version_from_name("v1"));
+    version cv2(c.version_from_name("v2"));
+    version cv3(c.version_from_name("v3"));
+
+    dep av1d1(*av1.deps_begin());
+    dep bv2d1(*bv2.deps_begin());
+    dep av2d1(*av2.deps_begin());
+    dep av3d1(*av3.deps_begin());
+
+
+    // Test sets:
+    // S0: (Install(av1, bv3, cv2))
+    // S1: (Install(av1 [av2d1], bv3, cv2))
+    // S2: (Install(av1 [av3d1], bv3, cv2))
+    // S3: (Install(cv2))
+    // S4: (Install(cv2), Break(av2d1))
+    // S5: (Break(av2d1), Break(bv2d1))
+    // S6: (Break(bv2d1))
+    //
+    // S0 contains S1
+    // S0 contains S2
+    // S0 contains S3
+    // S1 contains S3
+    // S2 contains S3
+    // S4 contains S3
+    // S5 contains S6
+    // Sn contains Sn for all n.
+
+    choice_set s0;
+    s0.insert(choice::make_install_version(av1));
+    s0.insert(choice::make_install_version(bv3));
+    s0.insert(choice::make_install_version(cv2));
+
+    choice_set s1;
+    s1.insert(choice::make_install_version_from_dep_source(av1, av2d1));
+    s1.insert(choice::make_install_version(bv3));
+    s1.insert(choice::make_install_version(cv2));
+
+    choice_set s2;
+    s2.insert(choice::make_install_version_from_dep_source(av1, av3d1));
+    s2.insert(choice::make_install_version(bv3));
+    s2.insert(choice::make_install_version(cv2));
+
+    choice_set s3;
+    s3.insert(choice::make_install_version(cv2));
+
+    choice_set s4;
+    s4.insert(choice::make_install_version(cv2));
+    s4.insert(choice::make_break_soft_dep(av2d1));
+
+    choice_set s5;
+    s5.insert(choice::make_break_soft_dep(av2d1));
+    s5.insert(choice::make_break_soft_dep(bv2d1));
+
+    choice_set s6;
+    s6.insert(choice::make_break_soft_dep(bv2d1));
+
+    std::vector<choice_set> sets;
+    sets.push_back(s0);
+    sets.push_back(s1);
+    sets.push_back(s2);
+    sets.push_back(s3);
+    sets.push_back(s4);
+    sets.push_back(s5);
+    sets.push_back(s6);
+
+    for(std::vector<choice_set>::size_type i = 0; i < sets.size(); ++i)
+      for(std::vector<choice_set>::size_type j = 0; j < sets.size(); ++j)
+	{
+	  std::ostringstream msg;
+	  msg << "In iteration (" << i << ", " << j << ")";
+
+	  if((i == j) ||
+	     (i == 0 && (j == 1 || j == 2 || j == 3)) ||
+	     (i == 1 && j == 3) ||
+	     (i == 2 && j == 3) ||
+	     (i == 4 && j == 3) ||
+	     (i == 5 && j == 6))
+	    CPPUNIT_ASSERT_MESSAGE(msg.str(), sets[i].contains(sets[j]));
+	  else
+	    CPPUNIT_ASSERT_MESSAGE(msg.str(), !sets[i].contains(sets[j]));
+	}
+  }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Choice_Set_Test);
