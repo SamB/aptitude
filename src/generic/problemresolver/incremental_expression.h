3232cd164d24850123a0c231455c6efa691edba3
Daniel Burrows <dburrows@debian.org>
2009-05-22 07:55:08 -0700

    Actually, take raw pointers in add_parent(). The reason here is that this will be invoked from constructors, which can't have smart pointers since the object isn't created yet.

--- src/generic/problemresolver/incremental_expression.h
+++ src/generic/problemresolver/incremental_expression.h
@@ -192,9 +192,10 @@ public:
   // Add a weak reference to the given expression; its
   // child_modified() routine will be invoked when this child's value
   // changes.
-  void add_parent(const cwidget::util::ref_ptr<expression_container<T> > &parent)
+  void add_parent(expression_container<T> *parent)
   {
-    parents.push_back(parent);
+    if(parent != NULL)
+      parents.push_back(parent);
   }
 
   virtual T get_value() = 0;
