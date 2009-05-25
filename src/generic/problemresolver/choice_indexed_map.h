e6dcb194c9011ddaa829102991025a0b94bdc0c0
Daniel Burrows <dburrows@debian.org>
2009-05-25 10:50:56 -0700

    Rework the choice-indexed-set stuff in terms of a choice-indexed-map instead. A map is a more natural fit for the semantics of the core data structure than a set, and the set behavior can easily be layered on top.  This will be very slightly less efficient (a few extra traversals of the set for some operations), but most of that can be recovered if it matters.

---
+++ src/generic/problemresolver/choice_indexed_map.h
@@ -0,0 +1,487 @@
+/** \file choice_indexed_set.h */    // -*-c++-*-
+
+
+//   Copyright (C) 2009 Daniel Burrows
+//
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License as
+//   published by the Free Software Foundation; either version 2 of
+//   the License, or (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//   General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; see the file COPYING.  If not, write to
+//   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+//   Boston, MA 02111-1307, USA.
+
+#ifndef CHOICE_INDEXED_SET_H
+#define CHOICE_INDEXED_SET_H
+
+#include "choice.h"
+
+#include <generic/util/immset.h>
+#include <generic/util/maybe.h>
+
+#include <functional>
+#include <utility>
+
+/** \brief A map from choices to objects, with support for iterating
+ *         over the objects associated with choices contained in a
+ *         particular choice.
+ *
+ *  \todo Various things could be made more efficient if this object
+ *  (and also imm::map) supported an update-in-place variant of
+ *  for_each.
+ *
+ *  \tparam ValueType   The value type of the map.
+ */
+template<typename PackageUniverse, typename ValueType >
+class generic_choice_indexed_map
+{
+  typedef typename PackageUniverse::package package;
+  typedef typename PackageUniverse::version version;
+  typedef typename PackageUniverse::dep dep;
+
+  typedef generic_choice<PackageUniverse> choice;
+
+  /** \brief Stores all the objects whose choice is scoped to a
+   *  particular dependency.
+   */
+  struct dep_info
+  {
+    maybe<ValueType> not_from_dep_source;
+    maybe<ValueType> from_dep_source;
+  };
+
+  /** \brief Represents information about all the objects whose
+   *  choices install a version.
+   */
+  struct version_info
+  {
+    maybe<ValueType> not_dep_scoped;
+    imm::map<dep, dep_info> dep_scoped;
+  };
+
+  // Objects stored for the choice (Install(v), t).
+  imm::map<version, version_info> install_version_objects;
+
+  // Objects stored for the choice (Break(d), t).
+  imm::map<dep, ValueType> break_dep_objects;
+
+public:
+  generic_choice_indexed_map()
+  {
+  }
+
+  void put(const choice &c, ValueType value)
+  {
+    switch(c.get_type())
+      {
+      case choice::install_version:
+	{
+	  typename imm::map<version, version_info>::node
+	    found_ver = install_version_objects.lookup(c.get_ver());
+
+	  version_info inf;
+	  if(found_ver.isValid())
+	    inf = found_ver.getVal();
+
+
+	  if(!c.get_dep_scoped())
+	    inf.not_dep_scoped = value;
+	  else
+	    {
+	      typename imm::map<dep, dep_info>::node
+		found_dep = inf.dep_scoped.lookup(c.get_dep());
+
+	      dep_info dep_inf;
+	      if(found_dep.isValid())
+		dep_inf = found_dep.getVal();
+
+	      if(c.get_from_dep_source())
+		dep_inf.from_dep_source = value;
+	      else
+		dep_inf.not_from_dep_source = value;
+
+	      inf.dep_scoped.put(c.get_dep(), dep_inf);
+	    }
+
+	  install_version_objects.put(c.get_ver(), inf);
+	}
+	break;
+
+      case choice::break_soft_dep:
+	break_dep_objects.put(c.get_dep(), value);
+	break;
+      }
+  }
+
+  /** \brief Retrieve the value bound to a choice.
+   *
+   *  \param c        The choice to look up.
+   *  \param output   A location in which to store the result.
+   *                  Only modified if c is contained in the map.
+   *
+   *  \return \b true if c was contained in this map, \b false
+   *  otherwise.
+   */
+  bool try_get(const choice &c, ValueType &output)
+  {
+    switch(c.get_type())
+      {
+      case choice::install_version:
+	{
+	  typename imm::map<version, version_info>::node
+	    found_ver = install_version_objects.lookup(c.get_ver());
+
+	  if(found_ver.isValid())
+	    {
+	      const version_info &ver_inf(found_ver.getVal());
+
+	      if(!c.get_dep_scoped())
+		{
+		  if(ver_inf.not_dep_scoped.get_has_value())
+		    {
+		      output = ver_inf.not_dep_scoped.get_value();
+		      return true;
+		    }
+		  else
+		    return false;
+		}
+	      else
+		{
+		  typename imm::map<dep, dep_info>::node
+		    found_dep = inf.dep_scoped.lookup(c.get_dep());
+
+		  dep_info dep_inf;
+		  if(found_dep.isValid())
+		    dep_inf = found_dep.getVal();
+
+		  if(c.get_from_dep_source())
+		    {
+		      if(dep_inf.from_dep_source.get_has_value())
+			{
+			  output = dep_inf.from_dep_source.get_value();
+			  return true;
+			}
+		      else
+			return false;
+		    }
+		  else
+		    {
+		      if(dep_inf.not_from_dep_source.get_has_value())
+			{
+			  output = dep_inf.not_from_dep_source.get_value();
+			  return true;
+			}
+		      else
+			return false;
+		    }
+		}
+	    }
+	  else
+	    return false;
+	}
+
+      case choice::break_soft_dep:
+	{
+	  typename imm::map<dep, ValueType>::node
+	    found_dep = break_dep_objects.lookup(c.get_dep());
+
+	  if(found_dep.isValid())
+	    output = found_dep.getVal();
+
+	  return found_dep.isValid();
+	}
+      }
+
+    return false;
+  }
+
+  /** \brief Remove a binding from this map.
+   *
+   *  Only an exact match will be erased.
+   */
+  void erase(const choice &c)
+  {
+    switch(c.get_type())
+      {
+      case choice::install_version:
+	{
+	  typename imm::map<version, version_info>::node
+	    found_ver = install_version_objects.lookup(c.get_ver());
+
+	  version_info inf;
+	  if(found_ver.isValid())
+	    inf = found_ver.getVal();
+
+
+	  if(!c.get_dep_scoped())
+	    inf.not_dep_scoped = maybe<ValueType>();
+	  else
+	    {
+	      typename imm::map<dep, dep_info>::node
+		found_dep = inf.dep_scoped.lookup(c.get_dep());
+
+	      dep_info dep_inf;
+	      if(found_dep.isValid())
+		dep_inf = found_dep.getVal();
+
+	      if(c.get_from_dep_source())
+		dep_inf.from_dep_source = maybe<ValueType>();
+	      else
+		dep_inf.not_from_dep_source = maybe<ValueType>();
+
+	      inf.dep_scoped.put(c.get_dep(), dep_inf);
+	    }
+
+	  install_version_objects.put(c.get_ver(), inf);
+	}
+	break;
+
+      case choice::break_soft_dep:
+	break_dep_objects.erase(c.get_dep());
+	break;
+      }
+  }
+
+  // Applies the given function to (choice, value) for each (choice,
+  // value) pair in the dependency scope that this object was applied
+  // to.
+  template<typename F>
+  struct for_each_by_dep_scope
+  {
+    version v;
+    F f;
+
+    for_each_by_dep_scope(const version &_v, F _f)
+      : f(_f), v(_v)
+    {
+    }
+
+    bool operator()(const std::pair<dep, dep_info> &scope) const
+    {
+      const dep &scope_d(scope.first);
+      const maybe<ValueType> &not_from_dep_source = scope.second.not_from_dep_source;
+      const maybe<ValueType> &from_dep_source = scope.second.from_dep_source;
+
+      if(not_from_dep_source.get_has_value())
+	{
+	  if(!f(choice::make_install_version(v, true, scope_d, -1),
+		not_from_dep_source.get_value()))
+	    return false;
+	}
+
+      if(from_dep_source.get_has_value())
+	{
+	  if(!f(choice::make_install_version_from_dep_source(v, scope_d, -1),
+		from_dep_source.get_value()))
+	    return false;
+	}
+    }
+  };
+
+  /** \brief Apply the given function object to (c', value) for each
+   *  mapping (c' -> value) in this set such that c' is contained in
+   *  c.
+   *
+   *  If f returns false, the iteration will abort.  Modifications to
+   *  this set after the iteration begins do not affect which values
+   *  are visited.
+   */
+  template<typename F>
+  bool for_each_key_contained_in(const choice &c, F f) const
+  {
+    switch(c.get_type())
+      {
+      case choice::install_version:
+	{
+	  const version &v(c.get_ver());
+	  typename imm::map<version, ver_info>::node
+	    found_ver(install_version_objects.find(v));
+	  // Note that since we save a reference to the original,
+	  // immutable node, we will always iterate over everything
+	  // even if the structure of the tree is changed.
+
+	  if(found_ver.isValid())
+	    {
+	      const version_inf &ver_inf(found_ver.getVal());
+
+	      if(!c.get_dep_scoped())
+		{
+		  // If the choice isn't dep-scoped, apply the
+		  // function to the not-dep-scoped cell and to
+		  // anything that's dep-scoped.
+		  for_each_with_choice<F>
+		    without_dep_f(choice::make_install_version(v, -1));
+
+		  if(ver_inf.not_dep_scoped.get_has_value())
+		    {
+		      if(!f(choice::make_install_version(v, -1),
+			    ver_inf.get_value()))
+			return false;
+		    }
+
+		  for_each_by_dep_scope<F> dep_scope_f(v, f);
+		  if(!ver_inf.dep_scoped.for_each(dep_scope_f))
+		    return false;
+		}
+	      else
+		{
+		  // If the choice is dep-scoped, find the particular
+		  // dependency that it applies to and apply f.
+		  typename imm::map<dep, dep_info>::node found_dep =
+		    ver_inf.dep_scoped.find(c.get_dep());
+
+		  if(found_dep.isValid())
+		    {
+		      const dep &d(c.get_dep());
+		      const dep_info &dep_inf(found_dep.getVal());
+
+		      if(!c.get_from_dep_source())
+			{
+			  // If it's not a from-dep-source dependency,
+			  // apply f to the not-from-dep-source cell.
+			  if(dep_inf.not_from_dep_source.get_has_value())
+			    {
+			      if(!f(choice::make_install_version(v, true, d, -1),
+				    dep_inf.not_from_dep_source.get_value()))
+				return false;
+			    }
+			}
+
+		      // Apply f to the from-dep-source cell.
+		      if(dep_inf.from_dep_source.get_has_value())
+			{
+			  if(!f(choice::make_install_version_from_dep_source(v, d, -1),
+				dep_inf.from_dep_source.get+value()))
+			    return false;
+			}
+		    }
+		}
+	    }
+	}
+	break;
+
+      case choice::break_soft_dep:
+	{
+	  const dep &d(c.get_dep());
+	  typename imm::map<dep, ValueType>::node
+	    found = break_dep_objects.lookup(d);
+
+	  if(found.isValid())
+	    {
+	      if(!f(choice::make_break_soft_dep(d, -1),
+		    found.getVal()))
+		return false;
+	    }
+	}
+	break;
+      }
+
+    return true;
+  }
+};
+
+/** \brief A set of objects, indexed for retrieval by an associated
+ *  choice.
+ *
+ *  This differs from choice_set in that it stores tuples (choice, T)
+ *  where "T" can be any LessThanComparable type.  It supports quickly
+ *  iterating over all the tuples for which the choice is contained in
+ *  an input choice.
+ *
+ *  \tparam T        The type of object stored in this set.
+ *  \tparam Compare  How to compare T objects.
+ */
+template<typename PackageUniverse, typename T, typename Compare = std::less<T> >
+class generic_choice_indexed_set
+{
+  generic_choice_indexed_map<PackageUniverse, imm::set<T, Compare> > parent;
+
+  // The comparison object.
+  Compare compare;
+
+public:
+  generic_choice_indexed_set(Compare _compare = Compare())
+    : compare(_compare)
+  {
+  }
+
+  /** \brief Insert a tuple (c, t) into this set. */
+  void insert(const choice &c, T t)
+  {
+    imm::set<T> values;
+    parent.try_get(c, values);
+
+    values.insert(t);
+    parent.put(c, values);
+  }
+
+  /** \brief Remove a tuple (c, t) from this set. */
+  void remove(const choice &c, T t)
+  {
+    imm::set<T> values;
+    if(parent.try_get(c, values))
+      {
+	values.erase(t);
+	if(values.empty())
+	  parent.erase(c);
+	else
+	  parent.put(c, values);
+      }
+  }
+
+  // Applies a function to each element of a set for a given choice.
+  template<typename F>
+  struct for_each_with_choice
+  {
+    choice c;
+    F f;
+
+    for_each_by_choice(const choice &_c, F _f)
+      : c(_c), f(_f)
+    {
+    }
+
+    bool operator()(T t) const
+    {
+      return f(c, t);
+    }
+  };
+
+  template<typename F>
+  struct for_each_set
+  {
+    F f;
+
+    for_each_set(F _f)
+      : F(_f)
+    {
+    }
+
+    bool operator()(const choice &c, const imm::set<T> &values) const
+    {
+      return values.for_each(for_each_with_choice<F>(c, f));
+    }
+  };
+
+  /** \brief Apply the given function object to (c', t) for each pair
+   *  (c', t) in this set such that c' is contained in c.
+   *
+   *  If f returns false, the iteration will abort.  Modifications to
+   *  this set after the iteration begins do not affect which values
+   *  are visited.
+   */
+  template<typename F>
+  bool for_each_contained_in(const choice &c, F f) const
+  {
+    parent.for_each_key_contained_in(c, for_each_set<F>(f));
+  }
+};
+
+#endif // CHOICE_INDEXED_SET_H
