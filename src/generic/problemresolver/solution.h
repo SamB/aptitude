9d2670301882443f4cb3c4db1bdbc2c204bfa977
Daniel Burrows <dburrows@debian.org>
2009-06-03 09:07:45 -0700

    Create joint score set choices as choices without a dependency, so that they don't segfault when printed.

--- src/generic/problemresolver/solution.h
+++ src/generic/problemresolver/solution.h
@@ -809,7 +809,7 @@ private:
       if(version == initial_state.version_of(version.get_package()))
 	any_is_current = true;
 
-      output.insert_or_narrow(choice::make_install_version(version, dep(), 0));
+      output.insert_or_narrow(choice::make_install_version(version, 0));
 
       return true;
     }
