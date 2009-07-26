5c2699c36e87ba6506450f18aec268af74cb2ab3
Daniel Burrows <dburrows@debian.org>
2009-07-26 14:57:54 -0700

    Add a skeletal unit tester using the Boost framework. The Boost framework is a lot nicer to work with than cppunit.  Rather than migrate over all at once, I'm going to start writing new tests with Boost, migrating old ones as I have time.

---
+++ tests/boost_test_main.cc
@@ -0,0 +1,9 @@
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+
+// One dummy test so that this can be dropped in before the actual
+// test suite is written.
+BOOST_AUTO_TEST_CASE(dummy)
+{
+}
