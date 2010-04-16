f1403f3b9d419c83c76ed3ababf3a49e2db0e3e8
Daniel Burrows <dburrows@debian.org>
2010-04-15 18:51:34 -0700

    Write a doccomment for cmdline_search().

--- src/cmdline/cmdline_search.h
+++ src/cmdline/cmdline_search.h
@@ -10,6 +10,38 @@
 /** \file cmdline_search.h
  */
 
+/** \brief Invoke the "search" and "versions" command-line actions.
+ *
+ *  \param argc             The number of entries in argv.
+ *
+ *  \param argv             The command-line arguments not parsed by main().
+ *
+ *  \param status_fname     If non-NULL, a file-name from which to read status
+ *                          information.
+ *
+ *  \param display_format   A string describing the columns to use in displaying
+ *                          results.
+ *
+ *  \param width            The width to use in formatting results.
+ *
+ *  \param sort             A string describing how to sort results, using the
+ *                          same syntax as the curses frontend.
+ *
+ *  \param disable_columns  \b true to disable columnar formatting and simply
+ *                          separate fields with whitespace.
+ *
+ *  \param debug            \b true to print debugging information to stdout.
+ *                          \todo  Should be handled by the logging subsystem.
+ *
+ *  \param search_versions  \b true to search for versions instead of packages.
+ *
+ *  \param group_by_package If search_versions is true, set this to \b true to
+ *                          group the output by package.
+ *
+ *  \todo Can this perhaps be refactored a bit so that search_versions and
+ *        search_packages can be passed in a more extensible way?  Probably
+ *        requires refactoring at the level of aptitude::matching too.
+ */
 int cmdline_search(int argc, char *argv[], const char *status_fname,
 		   std::string display_format, std::string width, std::string sort,
 		   bool disable_columns, bool debug,
