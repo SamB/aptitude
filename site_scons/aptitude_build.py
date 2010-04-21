d0392464e05837175b5beb81ed038d7ef0b43740
Daniel Burrows <dburrows@debian.org>
2010-04-21 08:30:54 -0700

    Better reporting of the status of the po4a check.

--- site_scons/aptitude_build.py
+++ site_scons/aptitude_build.py
@@ -125,12 +125,14 @@ def CheckForPo4A(context):
     """Look for po4a in $PO4APATH and set $PO4A accordingly.
 PO4APATH defaults to $PATH."""
     context.Message("Checking for po4a...")
+    context.Result("")
 
     po4apath = context.env.get('PO4APATH', '$PATH')
     for bindir in po4apath:
+        context.Message("  In %s..." % bindir)
         po4a = os.path.abspath(os.path.join(po4apath, po4a))
         if os.access(po4a, os.X_OK):
-            context.Result("%s" % po4a)
+            context.Result("yes")
             context.env['PO4A'] = po4a
             context.env['HAVE_PO4A'] = True
             return True
