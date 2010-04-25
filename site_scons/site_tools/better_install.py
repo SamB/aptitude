b81a60e571dc163cfccbba4d8d8b9e27053c1100
Daniel Burrows <dburrows@debian.org>
2010-04-25 14:56:52 -0700

    Fix DESTDIR handling to not be utterly broken.

--- site_scons/site_tools/better_install.py
+++ site_scons/site_tools/better_install.py
@@ -11,6 +11,22 @@ def generate(env):
     env.AddMethod(Install)
 
 def Install(env, targetdir, *sources):
+    def add_destdir(path):
+        '''Add DESTDIR to a path.
+
+os.path.join won't work because it doesn't join to absolute paths;
+join('a', '/b') returns '/b'.  And concatenation doesn't work right
+if DESTDIR lacks a trailing space.  This deals with both cases
+properly.'''
+        destdir = env['DESTDIR']
+        if destdir == '':
+            return path
+
+        if not destdir.endswith('/'):
+            destdir += '/'
+
+        return os.path.normpath('%s%s' % (destdir, path))
+
     # Only set up the alias if the user asked for an install, so
     # "scons ." works to build everything with no surprises and so
     # that multiple variants can be built without trying to create
@@ -18,10 +34,10 @@ def Install(env, targetdir, *sources):
     if 'install' in COMMAND_LINE_TARGETS:
         result = []
         for source in env.Flatten(sources):
-            output = os.path.join(targetdir, os.path.basename(Entry(source).path))
+            output = add_destdir(os.path.join(targetdir, os.path.basename(Entry(source).path)))
             result.append(env.Command(output,
                                       source,
-                                      Copy('$TARGET', '$DESTDIR$SOURCE')))
+                                      Copy('$TARGET', '$SOURCE')))
 
         env.Alias('install', result)
 
