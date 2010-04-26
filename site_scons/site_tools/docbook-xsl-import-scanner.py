06302278a1b296837d827ea5649fb06b3cfade49
Daniel Burrows <dburrows@debian.org>
2010-04-25 17:30:41 -0700

    Add custom scanners that pick out external entities in XML files and XSL imports. I had a terrible time getting SCons to accept explicit dependencies here (maybe because what I was trying to depend on didn't exist, but never mind that...); scanning for dependencies is easy, since Python has a built-in XML parser, and more robust than hardcoding what the deps "should" be.

---
+++ site_scons/site_tools/docbook-xsl-import-scanner.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2010 Daniel Burrows
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+# MA 02111-1307, USA.
+
+import xml.parsers.expat
+from SCons.Script import Scanner
+
+def exists():
+    return True
+
+def generate(env):
+    env.Append(SCANNERS = DocbookXSLImportScanner)
+
+def DocbookXSLImports(node, env, path):
+
+    result = []
+
+    def start_element(name, attrs):
+        if name == 'xsl:import':
+            href = attrs.get('href', None)
+            if href is None:
+                return
+
+            if href.startswith('http://'):
+                pass
+            elif href.startswith('file://'):
+                filename = href[len('file://'):]
+                result.append(filename)
+            else:
+                filename = href
+                result.append(filename)
+
+    try:
+        parser = xml.parsers.expat.ParserCreate()
+        parser.StartElementHandler = start_element
+
+        parser.ParseFile(file(node.path))
+    except xml.parsers.expat.ExpatError, e:
+        print e
+        # Probably an XML syntax error -- don't blow up the scanner
+        # for that!
+        pass
+
+    return result
+
+DocbookXSLImportScanner = Scanner(function = DocbookXSLImports,
+                                  skeys = [ '.xsl' ])
