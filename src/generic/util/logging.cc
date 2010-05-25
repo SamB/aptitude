2673726c52f41b474fc358f15a9ed4f62613ed79
Daniel Burrows <Daniel Burrows Daniel_Burrows@alumni.brown.edu>
2010-05-24 22:49:13 -0700

    Add a first draft of a logging library to replace liblog4cxx. Currently unused.  Needs test code.
    The reason for replacing log4cxx is that on the one hand, it
    provides much more functionality than aptitude needs in any
    conceivable universe, and on the other hand, it seems to be
    buggy and fragile.  Just initializing it takes a fairly careful
    dance, and it seems to be behind at least some of the sporadic
    crashes users are seeing in aptitude.  This new code does
    exactly what aptitude needs, in a simple and hopefully much
    more robust way (possibly a bit slower -- but the fast path
    should still be fast; any slowdown *should* be limited to
    the situation where we're logging a ton of data).

---
+++ src/generic/util/logging.cc
@@ -0,0 +1,335 @@
+/** \file logging.cc */
+
+
+// Copyright (C) 2010 Daniel Burrows
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+#include "logging.h"
+
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+
+#include <cwidget/generic/threads/threads.h>
+
+#include <iostream>
+
+using boost::enable_shared_from_this;
+using boost::make_shared;
+using boost::multi_index_container;
+using boost::multi_index::const_mem_fun;
+using boost::multi_index::hashed_non_unique;
+using boost::multi_index::hashed_unique;
+using boost::multi_index::indexed_by;
+using boost::multi_index::tag;
+using boost::optional;
+using boost::shared_ptr;
+using cwidget::threads::mutex;
+
+namespace aptitude
+{
+  namespace util
+  {
+    namespace logging
+    {
+      const char *describe_log_level(log_level l)
+      {
+        switch(l)
+          {
+          case TRACE_LEVEL: return "TRACE";
+          case DEBUG_LEVEL: return "DEBUG";
+          case INFO_LEVEL: return "INFO";
+          case WARN_LEVEL: return "WARN";
+          case ERROR_LEVEL: return "ERROR";
+          default: return "???";
+          }
+      }
+
+      class Logger::Impl : public Logger,
+                           public enable_shared_from_this<Logger::Impl>
+      {
+        // If set, the level that has been configured for this
+        // logger; otherwise, indicates that no level is configured
+        // (the logger will inherit its level from its parent).
+        optional<log_level> configuredLevel;
+
+        const std::string category;
+
+        const shared_ptr<Logger::Impl> parent;
+
+        sigc::signal<void, const char *, int, log_level, LoggerPtr, std::string>
+        signal_message_logged;
+
+        static log_level getDefaultLevel() { return ERROR_LEVEL; }
+
+        friend class Logger::LoggingSystem;
+
+      public:
+        Impl(const std::string &_category,
+             const shared_ptr<Logger::Impl> &_parent);
+
+        void log(const char *sourceFilename,
+                 int sourceLineNumber,
+                 log_level logLevel,
+                 const std::string &msg);
+
+        const std::string &getCategory() const;
+        const shared_ptr<Logger::Impl> &getParent() const;
+
+        void setLevel(const optional<log_level> &value);
+
+        sigc::connection
+        connect_message_logged(const sigc::slot<
+                                 void,
+                                 const char *,
+                                 int,
+                                 log_level,
+                                 LoggerPtr,
+                                 std::string> &slot);
+      };
+
+      Logger::Impl::Impl(const std::string &_category,
+                         const shared_ptr<Logger::Impl> &_parent)
+        : Logger(_parent.get() == NULL
+                 ? getDefaultLevel()
+                 : _parent->effectiveLevel),
+          category(_category),
+          parent(_parent)
+      {
+      }
+
+      void Logger::Impl::log(const char *sourceFilename,
+                             int sourceLineNumber,
+                             log_level logLevel,
+                             const std::string &msg)
+      {
+        signal_message_logged(sourceFilename,
+                              sourceLineNumber,
+                              logLevel,
+                              shared_from_this(),
+                              msg);
+      }
+
+      const std::string &Logger::Impl::getCategory() const
+      {
+        return category;
+      }
+
+      const shared_ptr<Logger::Impl> &Logger::Impl::getParent() const
+      {
+        return parent;
+      }
+
+      /** \brief Wrapper for the state of the logging system.
+       *
+       *  A singleton class is used to better encapsulate the state
+       *  and to handle lifetime issues properly.
+       */
+      class Logger::LoggingSystem
+      {
+        class by_category_tag;
+        class by_parent_tag;
+
+        typedef multi_index_container<
+          shared_ptr<Logger::Impl>,
+          indexed_by<
+            hashed_unique<tag<by_category_tag>,
+                          const_mem_fun<Logger::Impl,
+                                        const std::string &,
+                                        &Logger::Impl::getCategory> >,
+            hashed_non_unique<tag<by_parent_tag>,
+                              const_mem_fun<Logger::Impl,
+                                            const shared_ptr<Logger::Impl> &,
+                                            &Logger::Impl::getParent> > > >
+        logger_table;
+
+        typedef logger_table::index<by_category_tag>::type by_category_index;
+        typedef logger_table::index<by_parent_tag>::type by_parent_index;
+
+        logger_table loggers;
+
+        // Protects access to the table of loggers.  Although
+        // setLevel() is not thread-safe, the other public routines
+        // are, and some of them need to access the table.
+        mutex loggers_mutex;
+
+        typedef by_parent_index::const_iterator child_iterator;
+
+        /** \brief Find the immediate children of the given logger. */
+        std::pair<child_iterator, child_iterator>
+        find_children(const shared_ptr<Logger::Impl> &parent)
+        {
+          return loggers.get<by_parent_tag>().equal_range(parent);
+        }
+
+        /** \brief Set the effective level of the given logger and all
+         *  its descendants, stopping at proper descendants that don't
+         *  have a configured level.
+         *
+         *  \param logger  The starting logger.  This logger's effective
+         *                 level is always modified, even if it has a
+         *                 configured level.
+         *
+         *  \param effectiveLevel   The new effective level of logger
+         *                          and of its descendants.
+         */
+        void recursiveSetEffectiveLevel(const shared_ptr<Logger::Impl> &logger,
+                                        log_level effectiveLevel)
+        {
+          logger->effectiveLevel = effectiveLevel;
+          std::pair<child_iterator, child_iterator> children =
+            find_children(logger);
+
+          for(child_iterator it = children.first; it != children.second; ++it)
+            recursiveSetEffectiveLevel(*it, effectiveLevel);
+        }
+
+        LoggingSystem()
+        {
+        }
+
+        LoggingSystem(const LoggingSystem &);
+
+        // Like getLogger, but
+        //  1. Returns a Logger::Impl instead of a Logger;
+        //  2. Doesn't lock the mutex (so it can be used recursively
+        //     from within other routines).
+        shared_ptr<Logger::Impl> doGetLogger(const std::string &category);
+
+      public:
+        void setLevel(const shared_ptr<Logger::Impl> &category,
+                      const optional<log_level> &level);
+
+        LoggerPtr getLogger(const std::string &category);
+
+        void log(const char *sourceFilename,
+                 int sourceLineNumber,
+                 log_level logLevel,
+                 const std::string &msg);
+
+        static LoggingSystem &get()
+        {
+          static LoggingSystem system;
+
+          return system;
+        }
+      };
+
+      void Logger::LoggingSystem::setLevel(const shared_ptr<Logger::Impl> &category,
+                                           const optional<log_level> &level)
+      {
+        mutex::lock l(loggers_mutex);
+
+        // Update the category itself.
+        category->configuredLevel = level;
+
+        // If the configured level is now blank, we need to propagate
+        // the effective level from our parent (which is assumed to
+        // already be correct).  If we have no parent, we use Logger's
+        // default level.
+        log_level newEffectiveLevel;
+
+        if(!level)
+          {
+            const shared_ptr<Logger::Impl> parent = category->getParent();
+            if(parent.get() == NULL)
+              newEffectiveLevel = Logger::Impl::getDefaultLevel();
+            else
+              newEffectiveLevel = parent->effectiveLevel;
+          }
+        else
+          newEffectiveLevel = *level;
+
+        recursiveSetEffectiveLevel(category, newEffectiveLevel);
+      }
+
+      LoggerPtr Logger::LoggingSystem::getLogger(const std::string &category)
+      {
+        mutex::lock l(loggers_mutex);
+
+        return doGetLogger(category);
+      }
+
+      shared_ptr<Logger::Impl>
+      Logger::LoggingSystem::doGetLogger(const std::string &category)
+      {
+        by_category_index &by_category = loggers.get<by_category_tag>();
+
+        // First, try to find it directly.
+        {
+          by_category_index::const_iterator found =
+            by_category.find(category);
+
+          if(found != by_category.end())
+            return *found;
+        }
+
+        shared_ptr<Logger::Impl> parent;
+        // If they aren't asking for the root logger, find or
+        // instantiate the parent.
+        if(!category.empty())
+          {
+            // TODO: make "find the parent category name" a separate
+            // static method.
+            const std::string::size_type last_dot = category.rfind('.');
+            std::string parent_category_name;
+            if(last_dot != std::string::npos)
+              parent_category_name.assign(category, 0, last_dot);
+
+            parent = doGetLogger(parent_category_name);
+          }
+
+        shared_ptr<Logger::Impl> rval =
+          make_shared<Logger::Impl>(category, parent);
+
+        loggers.insert(rval);
+
+        return rval;
+      }
+
+      void Logger::Impl::setLevel(const optional <log_level> &value)
+      {
+        LoggingSystem::get().setLevel(shared_from_this(), value);
+      }
+
+      sigc::connection
+      Logger::Impl::connect_message_logged(const sigc::slot<
+                                             void,
+                                             const char *,
+                                             int,
+                                             log_level,
+                                             LoggerPtr,
+                                             std::string> &slot)
+      {
+        return signal_message_logged.connect(slot);
+      }
+
+
+      Logger::Logger(log_level _effectiveLevel)
+        : effectiveLevel(_effectiveLevel)
+      {
+      }
+
+      LoggerPtr Logger::getLogger(const std::string &category)
+      {
+        return LoggingSystem::get().getLogger(category);
+      }
+    }
+  }
+}
