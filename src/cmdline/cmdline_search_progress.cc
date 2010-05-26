084d8537e44de3f3ff47f9bed66faac2914ae8e5
Daniel_Burrows@alumni.brown.edu <Daniel_Burrows@alumni.brown.edu>
2010-05-25 20:59:37 -0700

    Add a first draft of some wrapper code to display search progress events. The design needs to be improved, but it's a proof-of-concept.
    The wrapper takes care of displaying which pattern is being searched for
    as part of the displayed progress message.  This has the side effect of
    causing the construction of progress messages to be a substantial fraction
    of the overall run-time.  The API needs to be redesigned so that messages
    are only formatted when they'll actually be displayed.

---
+++ src/cmdline/cmdline_search_progress.cc
@@ -0,0 +1,76 @@
+/** \file cmdline_search_progress.cc */
+
+
+// Copyright (C) 2010 Daniel Burrows
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; see the file COPYING.  If not, write to
+// the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+
+
+#include "cmdline_search_progress.h"
+
+#include "cmdline_progress_display.h"
+
+#include <generic/util/progress_info.h>
+
+
+#include <boost/format.hpp>
+
+#include <cwidget/generic/util/ref_ptr.h>
+
+using aptitude::util::progress_info;
+using aptitude::util::progress_type_bar;
+using aptitude::util::progress_type_none;
+using aptitude::util::progress_type_pulse;
+using boost::format;
+using boost::shared_ptr;
+using cwidget::util::ref_ptr;
+
+namespace aptitude
+{
+  namespace cmdline
+  {
+    void search_progress(const progress_info &info,
+                         const shared_ptr<progress_display> &progress_msg,
+                         const std::string &pattern)
+    {
+      // We interpret the progress_info to add a prefix to its message
+      // if it has one.
+      switch(info.get_type())
+        {
+        case progress_type_none:
+          progress_msg->set_progress(info, false);
+          break;
+
+        case progress_type_pulse:
+          progress_msg->set_progress(progress_info::pulse( (format("%s: %s")
+                                                            % pattern
+                                                            % info.get_progress_status())
+                                                           .str()),
+                                     false);
+          break;
+
+        case progress_type_bar:
+          progress_msg->set_progress(progress_info::bar(info.get_progress_fraction(),
+                                                        (format("%s: %s")
+                                                         % pattern
+                                                         % info.get_progress_status())
+                                                        .str()),
+                                     false);
+          break;
+        }
+    }
+  }
+}
