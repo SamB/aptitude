14761d6123807f361eee57380e9651256008636c
Daniel Burrows <Daniel Burrows Daniel_Burrows@alumni.brown.edu>
2010-06-06 18:41:23 -0700

    Fix the teletype mock object to handle characters that occupy two cells.

--- src/cmdline/mocks/teletype.cc
+++ src/cmdline/mocks/teletype.cc
@@ -37,6 +37,19 @@ namespace aptitude
   {
     namespace mocks
     {
+      namespace
+      {
+        // Convenience routine to treat negative widths as 0.
+        int safe_wcwidth(wchar_t c)
+        {
+          const int rval = wcwidth(c);
+          if(rval < 0)
+            return 0;
+          else
+            return rval;
+        }
+      }
+
       teletype::~teletype()
       {
       }
@@ -46,6 +59,9 @@ namespace aptitude
         class teletype_with_terminal : public teletype
         {
           std::wstring last_line;
+          // The index of the character containing the cursor:
+          unsigned int cursor_idx;
+          // The character cell containing the cursor:
           unsigned int cursor_position;
 
           const shared_ptr<terminal> term;
@@ -57,7 +73,9 @@ namespace aptitude
 
         public:
           teletype_with_terminal(const shared_ptr<terminal> &_term)
-            : cursor_position(0), term(_term)
+            : cursor_idx(0),
+              cursor_position(0),
+              term(_term)
           {
             ON_CALL(*term, output(_))
               .WillByDefault(Invoke(this, &teletype_with_terminal::handle_output));
@@ -82,6 +100,7 @@ namespace aptitude
         void teletype_with_terminal::scroll_line()
         {
           last_line.clear();
+          cursor_idx = 0;
           cursor_position = 0;
           newline();
         }
@@ -98,7 +117,7 @@ namespace aptitude
           for(std::wstring::const_iterator it = output.begin();
               it != output.end(); ++it)
             {
-              const char c = *it;
+              const wchar_t c = *it;
 
               switch(c)
                 {
@@ -110,42 +129,90 @@ namespace aptitude
                   break;
 
                 case '\r':
+                  cursor_idx = 0;
                   cursor_position = 0;
                   break;
 
                 default:
-                  if(cursor_position == screen_width)
-                    {
-                      if(!suppress_set_last_line)
-                        set_last_line(last_line);
-                      scroll_line();
-                      suppress_set_last_line = true;
+                  {
+                    const int c_width = safe_wcwidth(c);
 
-                      // Now that we've moved to the next line, we
-                      // still need to output the character that
-                      // caused the scroll.  This breaks if the screen
-                      // width is 0 -- don't do that.
-                      last_line.push_back(c);
-                      ++cursor_position;
-                    }
-                  else
-                    {
-                      suppress_set_last_line = false;
+                    if(cursor_position + c_width > screen_width)
+                      {
+                        if(!suppress_set_last_line)
+                          set_last_line(last_line);
+                        scroll_line();
+                        // Don't suppress the last line, since we're
+                        // about to output a character (making it
+                        // nonempty).
+                        suppress_set_last_line = false;
 
-                      if(cursor_position < last_line.size())
-                        last_line[cursor_position] = c;
-                      else
-                        {
-                          // Note that cursor_position should be at
-                          // most last_line.size() + 1; this is just
-                          // for safety's sake.
-                          while(last_line.size() + 1 < cursor_position)
-                            last_line.push_back(' ');
+                        // Now that we've moved to the next line, we
+                        // still need to output the character that
+                        // caused the scroll.  This breaks if the screen
+                        // width is 0 -- don't do that.
+                        last_line.push_back(c);
+                        cursor_position += c_width;
+                        ++cursor_idx;
+                      }
+                    else
+                      {
+                        suppress_set_last_line = false;
 
-                          last_line.push_back(c);
-                        }
-                      ++cursor_position;
-                    }
+                        if(cursor_idx < last_line.size())
+                          {
+                            // This is an overwrite.  Find the character
+                            // or characters that were overwritten and
+                            // replace them with the new character.  If
+                            // some of the cells containing the old
+                            // character aren't filled, fill them with
+                            // spaces.
+                            //
+                            // Note that we don't handle the case where
+                            // we start writing in the middle of a
+                            // character, since that can't happen (since
+                            // the cursor starts at the left and only
+                            // moves via writes).
+                            int chars_to_replace = 0;
+                            int replaced_width = 0;
+
+                            while(replaced_width < c_width)
+                              {
+                                const int replace_idx =
+                                  cursor_idx + chars_to_replace;
+                                const int previous_width =
+                                  safe_wcwidth(last_line[replace_idx]);
+
+                                ++chars_to_replace;
+                                replaced_width += previous_width;
+                              }
+
+                            std::wstring new_text;
+                            new_text.push_back(c);
+
+                            for(int i = c_width; i < replaced_width; ++i)
+                              new_text.push_back(L' ');
+
+
+                            last_line.replace(cursor_idx,
+                                              chars_to_replace,
+                                              new_text);
+                          }
+                        else
+                          {
+                            // Note that cursor_idx should be at
+                            // most last_line.size() + 1; this is just
+                            // for safety's sake.
+                            while(last_line.size() + 1 < cursor_idx)
+                              last_line.push_back(' ');
+
+                            last_line.push_back(c);
+                          }
+
+                        cursor_position += c_width;
+                        ++cursor_idx;
+                      }
+                  }
                 }
             }
 
@@ -156,6 +223,7 @@ namespace aptitude
         void teletype_with_terminal::handle_move_to_beginning_of_line()
         {
           cursor_position = 0;
+          cursor_idx = 0;
         }
       }
 
