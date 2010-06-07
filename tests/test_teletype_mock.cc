a3359c1cc3f71be9e486da0ac591fb7eb1512dfa
Daniel Burrows <Daniel Burrows Daniel_Burrows@alumni.brown.edu>
2010-06-06 17:15:51 -0700

    Write tests verifying the behavior of the teletype mock object with respect to wide characters. Note that in order to do this, I *had to modify LC_CTYPE for the duration of the test*.  There doesn't seem to be a way around this (short of mocking out the locale system, which would be nuts), and we might have to do the same thing in tests of other parts of the system.
    Currently these tests fail, because the teletype mock doesn't handle fullwidth
    characters.

--- tests/test_teletype_mock.cc
+++ tests/test_teletype_mock.cc
@@ -24,8 +24,12 @@
 #include <cmdline/mocks/terminal.h>
 
 // System includes:
+#include <cwidget/generic/util/transcode.h>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#include <locale.h>
 
 namespace mocks = aptitude::cmdline::mocks;
 
@@ -37,17 +41,73 @@ using testing::Test;
 
 namespace
 {
+  const wchar_t two_column_char = L'\uff01';
+
   struct TeletypeTest : public Test
   {
+    // An arbitrary Unicode codepoint that I happen to know will take
+    // two columns.  (TODO: does this make the test locale-dependent?
+    // Although using some sort of locale mock is kind of a scary idea)
+    const std::string widechar;
+
+    // Stores the previous value of LC_CTYPE before we modified it for
+    // the test.
+    //
+    // I really, really don't like having to do this.  But mocking out
+    // the whole locale system would be utterly insane, and I can't
+    // find a Unicode character that I can test regardless of the
+    // value of CTYPE.
+    std::string previous_lc_ctype;
+
     shared_ptr<mocks::terminal> term;
     shared_ptr<mocks::teletype> teletype;
 
+    static std::string safe_string(const char *c)
+    {
+      if(c == NULL)
+        return std::string();
+      else
+        return c;
+    }
+
+    // narrow -> wide conversion function that doesn't depend on the
+    // current locale.
+    static std::wstring transcode(const std::string &s)
+    {
+      return cwidget::util::transcode(s, "UTF-8");
+    }
+
+    // wide -> narrow conversion function that doesn't depend on the
+    // current locale.
+    static std::string transcode(const std::wstring &s)
+    {
+      return cwidget::util::transcode(s, "UTF-8");
+    }
+
     TeletypeTest()
-      : term(mocks::create_combining_terminal()),
+      : widechar(transcode(std::wstring(1, two_column_char))),
+        term(mocks::create_combining_terminal()),
         teletype(mocks::create_teletype(term))
     {
       EXPECT_CALL(*term, get_screen_width())
         .WillRepeatedly(Return(80));
+    }
+
+    void SetUp()
+    {
+      previous_lc_ctype = safe_string(setlocale(LC_CTYPE, NULL));
+      setlocale(LC_CTYPE, "zh_CN.UTF-8");
+
+      // Sanity-check the widechar string.
+      const std::wstring widechar_w = transcode(widechar);
+      EXPECT_EQ(1, widechar_w.size());
+      if(widechar_w.size() >= 0)
+        {
+          EXPECT_EQ(two_column_char, widechar_w[0]);
+          EXPECT_TRUE(iswprint(widechar_w[0]));
+        }
+      EXPECT_EQ(1, wcslen(widechar_w.c_str()));
+      EXPECT_EQ(2, wcswidth(widechar_w.c_str(), widechar_w.size()));
     }
   };
 }
@@ -98,6 +158,60 @@ TEST_F(TeletypeTest, testOverwriteOneCharAtATime)
   term->flush();
 }
 
+TEST_F(TeletypeTest, OverwriteNarrowCharWithWideChar)
+{
+  {
+    InSequence dummy;
+    EXPECT_CALL(*teletype, set_last_line("abc"));
+    EXPECT_CALL(*teletype, set_last_line(widechar + "c"));
+  }
+
+  term->output("abc");
+  term->move_to_beginning_of_line();
+  term->flush();
+
+  term->output(widechar);
+  term->flush();
+}
+
+TEST_F(TeletypeTest, OverwriteWideCharWithNarrowChar)
+{
+  {
+    InSequence dummy;
+    EXPECT_CALL(*teletype, set_last_line(widechar + "c"));
+    // NB: this behavior isn't quite what a real terminal will do, but
+    // it's close enough for testing. (the terminal would have a blank
+    // space without an actual character, but it would be too
+    // complicated to simulate that, and it doesn't really matter from
+    // the point of view of seeing what the output looks like, which
+    // is what this is for)
+    EXPECT_CALL(*teletype, set_last_line("a c"));
+  }
+
+  term->output(widechar + "c");
+  term->move_to_beginning_of_line();
+  term->flush();
+
+  term->output("a");
+  term->flush();
+}
+
+TEST_F(TeletypeTest, OverwriteWideCharWithNarrowChars)
+{
+  {
+    InSequence dummy;
+    EXPECT_CALL(*teletype, set_last_line(widechar + "c"));
+    EXPECT_CALL(*teletype, set_last_line("abc"));
+  }
+
+  term->output(widechar + "c");
+  term->move_to_beginning_of_line();
+  term->flush();
+
+  term->output("ab");
+  term->flush();
+}
+
 TEST_F(TeletypeTest, overwriteEverything)
 {
   {
@@ -144,6 +258,53 @@ TEST_F(TeletypeTest, testWritePastEOL)
   }
 
   term->output("abcdefghij");
+}
+
+TEST_F(TeletypeTest, WritePastEOLAfterWideChar)
+{
+  EXPECT_CALL(*term, get_screen_width())
+    .WillRepeatedly(Return(4));
+
+  {
+    InSequence dummy;
+
+    EXPECT_CALL(*teletype, set_last_line(widechar + "bc"));
+    EXPECT_CALL(*teletype, newline());
+    EXPECT_CALL(*teletype, set_last_line("def"));
+  }
+
+  term->output(widechar + "bcdef");
+}
+
+TEST_F(TeletypeTest, WriteWideCharPastEOL)
+{
+  EXPECT_CALL(*term, get_screen_width())
+    .WillRepeatedly(Return(4));
+
+  {
+    InSequence dummy;
+
+    EXPECT_CALL(*teletype, set_last_line(widechar + widechar));
+    EXPECT_CALL(*teletype, newline());
+    EXPECT_CALL(*teletype, set_last_line(widechar));
+  }
+
+  term->output(widechar + widechar + widechar);
+}
+
+TEST_F(TeletypeTest, WriteWideCharPastEOLWithSplit)
+{
+  EXPECT_CALL(*term, get_screen_width())
+    .WillRepeatedly(Return(4));
+
+  {
+    InSequence dummy;
+    EXPECT_CALL(*teletype, set_last_line("a" + widechar));
+    EXPECT_CALL(*teletype, newline());
+    EXPECT_CALL(*teletype, set_last_line(widechar + "a"));
+  }
+
+  term->output("a" + widechar + widechar + "a");
 }
 
 TEST_F(TeletypeTest, testOverwritePastEOL)
